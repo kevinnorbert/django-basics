88f567c7c3d69709712148a72e0bcff74444efe7
Kevin Norbert <kevin.norbert@acelrtech.com>
2021-08-04 15:29:40 +0530

    Create Product added

--- product/models.py
+++ product/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
 # Create your models here.
-class Products:
+class Product(models.Model):
     name = models.CharField(max_length=100)
     code = models.CharField(max_length=100, unique=True)
-    unit_price = models.DecimalField()
+    unit_price = models.DecimalField(decimal_places=2, max_digits=10)
 
--- product/views.py
+++ product/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse, response
+from django.views.decorators.csrf import csrf_exempt
+from django.db import IntegrityError
+import json
+from product.models import Product
 # Create your views here.
+@csrf_exempt
+def create_product(request):
+    body = request.body
+    request_json = json.loads(body)
+    product_object = Product()
+    try:
+        product_object.name = request_json['name']
+        product_object.code = request_json['code']
+        product_object.unit_price = request_json['unit_price']
+        product_object.save()
+        response = {
+        'message': 'Created successfully',
+        'code': 200,
+        'id': product_object.id
+        }
+    except IntegrityError:
+        response = {
+        'message': 'Product with same code already exists',
+        'code': 500,
+        }
+    return JsonResponse(response, safe=False)
+
+@csrf_exempt
+def fetch_product(request):
+    pass
+
+@csrf_exempt
+def fetch_single_product(request):
+    pass
+
+@csrf_exempt
+def update_product(request):
+    pass
+
+@csrf_exempt
+def delete_product(request):
+    pass
