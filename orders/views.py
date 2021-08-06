62e1fb49c937b3442675b41e2ff5fbc4867c7f6a
Kevin Norbert <kevin.norbert@acelrtech.com>
2021-08-06 18:06:26 +0530

    Finished but sussy

--- orders/views.py
+++ orders/views.py
@@ -15,42 +15,67 @@ def create_order(request):
     body = request.body
     request_json = json.loads(body)
     customer_name = request_json['customer_name']
-    try:
-        customer_objects = Customer.objects.all()
-        for customer_object in customer_objects:
-            if customer_object.fullname == customer_name:
-                customer_exists = True
-                break
-        if customer_exists:
-            lines = request_json['lines']
-            for line in lines:
-                request_product_code = line['product_code']
-                product_objects = Product.objects.all()
-                product_code_found = False
-                for product_object in product_objects:
-                    if product_object.code == request_product_code:
-                        product_code_found = True
-                        break
-                if not product_code_found:
-                    product_code_exists = False
-                    response = {
-                        "message": f"Product {request_product_code} does not exist",
-                        "status": False
-                    }
+    
+    customer_objects = Customer.objects.all()
+    for customer_object in customer_objects:
+        if customer_object.fullname == customer_name:
+            customer_exists = True
+            break
+    if customer_exists:
+        lines = request_json['lines']
+        for line in lines:
+            request_product_code = line['product_code']
+            product_objects = Product.objects.all()
+            product_code_found = False
+            for product_object in product_objects:
+                if product_object.code == request_product_code:
+                    product_code_found = True
                     break
-            if product_code_exists:
-                pass
+            if not product_code_found:
+                product_code_exists = False
+                response = {
+                    "message": f"Product {request_product_code} does not exist",
+                    "status": False
+                }
+                break
+        if product_code_exists:
+            try:
+                order_object = Order()
+                order_object.customer_name = customer_name
+                order_object.order_no = request_json['order_no']
+                order_object.grand_total = request_json['grand_total']
+                order_object.save()
+                for line in lines:
+                    order_line_object = OrderLine()
+                    order_line_object.order_id = order_object.id
+                    order_line_object.product_name = line['product_name']
+                    order_line_object.product_code = line['product_code']
+                    order_line_object.unit_price = line['unit_price']
+                    order_line_object.qty = line['qty']
+                    order_line_object.tax_rate = line['tax_rate']
+                    order_line_object.save()
                 
-        else:
-            response = {
-            'message': "Customer doesn't exists",
-            'code': 500,
-            }
-    except IntegrityError:
+                response = {
+                    "message": "Order saved successfully",
+                    "status": True
+                }
+            except IntegrityError:
+                response = {
+                    "message": "Order already exists",
+                    "status": True
+                }
+
+
+                    
+
+
+
+    else:
         response = {
-            'message': 'Order no: already exists',
-            'code': 500
+        'message': "Customer doesn't exists",
+        'code': 500,
         }
+    
     return JsonResponse(response, safe=False)
     
 
